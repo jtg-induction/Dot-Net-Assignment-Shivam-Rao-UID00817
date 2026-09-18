9d05e1bfd9b8fe1d1739b4c3a96adbdc073a9b4d
Shivam Rao <shivam.rao@joshtechnologygroup.com>
2026-09-18 12:25:14 +0530

    fixed validationexception class

--- Dot-Net-Assignment-Shivam-Rao-UID00817/Exceptions/ValidationException.cs
+++ Dot-Net-Assignment-Shivam-Rao-UID00817/Exceptions/ValidationException.cs
@@ -9,7 +9,7 @@ namespace Dot_Net_Assignment_Shivam_Rao_UID00817.Exceptions
         
         public ValidationException(string message) : base(message)
         {
-            ValidationMessages.Add(message);
+            this.ValidationMessages = new List<String> { message };
         }
 
         public ValidationException(List<String> validationMessages)
